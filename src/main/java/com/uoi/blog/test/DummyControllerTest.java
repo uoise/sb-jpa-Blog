cde5f56ec5dea6e2a46caba25049c82c15a73e11
uoise <yw823x@daum.net>
2021-06-28 23:05:46 +0900

    Paging User

--- src/main/java/com/uoi/blog/test/DummyControllerTest.java
+++ src/main/java/com/uoi/blog/test/DummyControllerTest.java
@@ -4,10 +4,16 @@ import com.uoi.blog.model.RoleType;
 import com.uoi.blog.model.User;
 import com.uoi.blog.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 // http파일이 아닌 data를 리턴해주는 controller
 @RestController
@@ -15,7 +21,22 @@ public class DummyControllerTest {
 
     @Autowired // DI
     private UserRepository userRepository;
-    
+
+    // http://localhost:8000/blog/dummy/user/
+    @GetMapping("/dummy/users")
+    public List<User> list(){
+        return userRepository.findAll();
+    }
+
+    // 한 페이지 당 2건의 데이터 리턴, 최신순으로 정렬
+    @GetMapping("/dummy/user")
+    public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
+        Page<User> pagingUser = userRepository.findAll(pageable);
+        // Page<>를 통해 isFirst, isLast등의 로직 사용가능
+        List<User> users = pagingUser.getContent();
+        return users;
+    }
+
     // {id}주소로 파라미터 전달받기 가능
     // http://localhost:8000/blog/dummy/user/3
     @GetMapping("/dummy/user/{id}")
