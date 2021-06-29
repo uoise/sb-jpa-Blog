81418997564ae402fd33f8ef06803f36357cc321
uoise <yw823x@daum.net>
2021-06-29 21:17:27 +0900

    Update User

--- src/main/java/com/uoi/blog/test/DummyControllerTest.java
+++ src/main/java/com/uoi/blog/test/DummyControllerTest.java
@@ -8,11 +8,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 // http파일이 아닌 data를 리턴해주는 controller
@@ -22,12 +20,34 @@ public class DummyControllerTest {
     @Autowired // DI
     private UserRepository userRepository;
 
+    // password, email만 수정
+    @Transactional
+    @PutMapping("/dummy/user/{id}")
+    public User updateUser(@PathVariable int id, @RequestBody User requestUser){
+    // json 데이터로 요청 @RequestBody
+    // jackson lib이 java obj로 변환
+        System.out.println("id : "+ id);
+        System.out.println("password : "+ requestUser.getPassword());
+        System.out.println("email : "+ requestUser.getEmail());
+
+        User user = userRepository.findById(id).orElseThrow(()->{
+            return new IllegalArgumentException("수정에 실패하였습니다");
+        });
+        user.setPassword(requestUser.getPassword());
+        user.setEmail(requestUser.getEmail());
+//        userRepository.save(user);
+        // save 함수는 id를 전달하지 않거나, id에 대한 데이터가 없으면 insert, 있으면 update
+        return user;
+    }// @Transactional -> 더티 체킹, 값만 변경하면 save필요없이 변경됨
+    // 함수 종료(리턴)시 자동 commit
+
     // http://localhost:8000/blog/dummy/user/
     @GetMapping("/dummy/users")
     public List<User> list(){
         return userRepository.findAll();
     }
 
+    // http://localhost:8000/blog/dummy/user/3
     // 한 페이지 당 2건의 데이터 리턴, 최신순으로 정렬
     @GetMapping("/dummy/user")
     public List<User> pageList(@PageableDefault(size = 2, sort = "id", direction = Sort.Direction.DESC)Pageable pageable){
